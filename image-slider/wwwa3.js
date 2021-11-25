70f2bf695df458b5719261cfe78a5999362ae67a
Natalie <natalie.wong@invbots.com>
2021-11-25 22:46:25 +0800

    submit

--- image-slider/wwwa3.js
+++ image-slider/wwwa3.js
@@ -1,72 +1,52 @@
 const imageFolder = "images/";
-const fps = 60;
 let timeouts = [];
-const scaleToFit = (img, canvas, ctx) => {
-    // get the scale
-    let scale = Math.min(canvas.width / img.width, canvas.height / img.height);
-    // get the top left position of the image
-    let x = (canvas.width / 2) - (img.width / 2) * scale;
-    let y = (canvas.height / 2) - (img.height / 2) * scale;
-    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
-}
-
 window.addEventListener('DOMContentLoaded', () => {
-
     for (let i = 0; i < images.length; i++) {
         images[i] = imageFolder + images[i]
     }
-    images.forEach(
-        item => console.log(item)
-    );
+    // images.forEach(
+    //     item => console.log(item)
+    // );
     let canvas = document.getElementById("myCanvas");
     let context = canvas.getContext("2d");
-    // let image = new Image();
-    // let transitionCounter = 0;
-    // image.src = images[0];
-    // image.onload = function () {
-    //     scaleToFit(image, canvas, context);
-    // };
-    console.log("loading");
     let imageObjs = [];
-    let imageLoaded = 0;
-    let imageIndex = 0;
-    let transitionCounter = 0;
+    let loadImgCounter = 0;
+    let index = 0;
+    let fadeInCounter = 0;
     for(let i = 0; i < images.length; i++){
-        console.log(i);
         let image = new Image();
         image.src = images[i];
         image.onload = function () {
-            console.log(`loaded ${i}th img`);
-            imageLoaded++;
-            console.log("imageLoaded: ", imageLoaded);
-            if(imageLoaded === images.length){
+            loadImgCounter++;
+            if(loadImgCounter === images.length){
                 console.log("loaded all");
                 timeouts = transitionImage();
             }
         }
-        imageObjs.push(image);
+        imageObjs[i] = image;
     }
     const transitionImage =() => {
         timeouts[timeouts.length] = setTimeout(function animate() {
             window.requestAnimationFrame(transitionImage);
-            let image = imageObjs[imageIndex];
-            let scale = Math.min(canvas.width / image.width, canvas.height / image.height);
-            let x = (canvas.width / 2) - (image.width / 2) * scale * transitionCounter;
-            let y = (canvas.height / 2) - (image.height / 2) * scale;
-            context.globalAlpha = transitionCounter;
+            let currentImg = imageObjs[index];
+            let scale = Math.min(canvas.width / currentImg.width, canvas.height / currentImg.height);
+            let x = (canvas.width / 2) - (currentImg.width / 2) * scale * fadeInCounter;
+            let y = (canvas.height / 2) - (currentImg.height / 2) * scale;
+            let newWidth = currentImg.width * scale;
+            let newHeight = currentImg.height * scale;
+            context.globalAlpha = fadeInCounter;
             context.clearRect( 0,0,canvas.width,canvas.height);
-            // context.drawImage(image,x,y);
             context.fillStyle = 'white';
-            context.drawImage(image, x, y, image.width * scale, image.height * scale);
-            transitionCounter+=.01;
-            if(transitionCounter>=1.00){
-                transitionCounter=0.00;
-                imageIndex++;
-                if(imageIndex>=images.length){
-                    imageIndex=0;
+            context.drawImage(currentImg, x, y, newWidth, newHeight);
+            fadeInCounter += .01;
+            if(fadeInCounter >= 1.00){
+                index++;
+                fadeInCounter = 0.00;
+                if(index > imageObjs.length){
+                    index=0;
                 }
             }
-        }, 3000/fps);
+        }, 3000/60);
         return timeouts;
     };
     function stopCheck(timeouts) {
@@ -75,12 +55,18 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    canvas.addEventListener("mouseenter", function () {
-        console.log("enter");
+    canvas.addEventListener("mouseover", function () {
+        // console.log("enter");
         stopCheck(timeouts);
     });
     canvas.addEventListener("mouseleave", function () {
-        console.log("leave");
+        // console.log("leave");
+        stopCheck(timeouts);
+        transitionImage();
+    });
+    canvas.addEventListener("click", function () {
+        // console.log("clicked");
+        stopCheck(timeouts);
         transitionImage();
     });
 });
